23acd9122bbd3984db21a429d9fdaafddfbcce33
IlyaGit95 <ilyagit95@mail.ru>
2023-12-08 18:50:26 +0300

    CRUDpack

--- src/entities/user/user.controller.ts
+++ src/entities/user/user.controller.ts
@@ -1,121 +1,125 @@
 import { Controller, Get, Post, Req, Res, Put, Delete, Param, ParseIntPipe, Body, HttpException, HttpStatus, NotFoundException } from '@nestjs/common'
 import { Response, Request } from 'express'
 import { ApiBody, ApiTags, ApiResponse } from '@nestjs/swagger';
+import { Crud, CrudController } from "@nestjsx/crud";
 
 import { UserService } from './user.service'
 import { UpdateUserDto } from './dto/updateUser.dto'
 import { User } from './user.entity';
 
+@Crud({
+  model: {
+    type: User,
+  },
+})
 @ApiTags('Users')
 @Controller('users')
-export class UserController {
-  constructor(
-     private readonly userService: UserService,
-  ) {}
+export class UserController implements CrudController<User> {
+  constructor(public service: UserService) {}
  
-  @Get('/')
-  @ApiResponse({ 
-    status: 200, 
-    description: 'Get all users.',
-    isArray: true
-  })
-  async getAllUsers(
-    @Res() res: Response,
-  ) {
-     const users = await this.userService.getAllUsers()
+  // @Get('/')
+  // @ApiResponse({ 
+  //   status: 200, 
+  //   description: 'Get all users.',
+  //   isArray: true
+  // })
+  // async getAllUsers(
+  //   @Res() res: Response,
+  // ) {
+  //    const users = await this.userService.getAllUsers()
 
-     return res.send({
-      status: 'ok',
-      data: users,
-    })
-  }
+  //    return res.send({
+  //     status: 'ok',
+  //     data: users,
+  //   })
+  // }
 
-  @Get('/:id')
-  @ApiResponse({ 
-    status: 200, 
-    description: 'Get user by id.',
-    type: User
-  })
-  @ApiResponse({ 
-    status: 404, 
-    description: 'User not Found.'
-  })
-  async getUser(
-    @Param('id', ParseIntPipe) id: number,
-    @Res() res: Response,
-  ) {
-     const userData = await this.userService.getUserData(id)
+  // @Get('/:id')
+  // @ApiResponse({ 
+  //   status: 200, 
+  //   description: 'Get user by id.',
+  //   type: User
+  // })
+  // @ApiResponse({ 
+  //   status: 404, 
+  //   description: 'User not Found.'
+  // })
+  // async getUser(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @Res() res: Response,
+  // ) {
+  //    const userData = await this.userService.getUserData(id)
 
-     if(userData === null) {
-      throw new HttpException('User with id = ' + id + ' not found', HttpStatus.NOT_FOUND)
-     }
+  //    if(userData === null) {
+  //     throw new HttpException('User with id = ' + id + ' not found', HttpStatus.NOT_FOUND)
+  //    }
 
-    return res.send({
-      status: 'ok',
-      data: userData,
-    })
-  }
+  //   return res.send({
+  //     status: 'ok',
+  //     data: userData,
+  //   })
+  // }
 
-  @Post('/')
-  @ApiResponse({ 
-    status: 200, 
-    description: 'Create user.',
-    type: User
-  })
-  @ApiBody({ type: UpdateUserDto })
-  async createUser(
-    @Req() req: Request,
-    @Res() res: Response,
-  ) {
-    await this.userService.createUser(req.body)
-    console.log(req.body)
-    return res.send({ status: 'ok' })
-  }
+  // @Post('/')
+  // @ApiResponse({ 
+  //   status: 200, 
+  //   description: 'Create user.',
+  //   type: User
+  // })
+  // @ApiBody({ type: UpdateUserDto })
+  // async createUser(
+  //   @Req() req: Request,
+  //   @Res() res: Response,
+  // ) {
+  //   await this.userService.createUser(req.body)
+  //   console.log(req.body)
+  //   return res.send({ status: 'ok' })
+  // }
 
-  @Put('/:id')
-  @ApiResponse({ 
-    status: 200, 
-    description: 'Update user.',
-    type: User
-  })
-  @ApiResponse({ 
-    status: 404,  
-    description: 'User not Found.'
-  })
-  @ApiBody({ type: UpdateUserDto })
-  async updateUser(
-    @Param('id', ParseIntPipe) id: number,
-    @Body() body: UpdateUserDto,
-    @Res() res: Response,
-  ) {
+  // @Put('/:id')
+  // @ApiResponse({ 
+  //   status: 200, 
+  //   description: 'Update user.',
+  //   type: User
+  // })
+  // @ApiResponse({ 
+  //   status: 404,  
+  //   description: 'User not Found.'
+  // })
+  // @ApiBody({ type: UpdateUserDto })
+  // async updateUser(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @Body() body: UpdateUserDto,
+  //   @Res() res: Response,
+  // ) {
      
-    const userData = await this.userService.getUserData(id)
-     if(userData === null) {
-      throw new HttpException('User with id = ' + id + ' not found', HttpStatus.NOT_FOUND)
-     }
+  //   const userData = await this.userService.getUserData(id)
+  //    if(userData === null) {
+  //     throw new HttpException('User with id = ' + id + ' not found', HttpStatus.NOT_FOUND)
+  //    }
 
-    await this.userService.updateUserData(id, body)
-    return res.send({ status: 'ok' })
-  }
+  //   await this.userService.updateUserData(id, body)
+  //   return res.send({ status: 'ok' })
+  // }
 
-  @Delete('/:id')
-  @ApiResponse({ 
-    status: 200, 
-    description: 'Delete user.'
-  })
-  @ApiResponse({ 
-    status: 404, 
-    description: 'User not Found.'
-  })
-  async deleteUser(
-    @Param('id', ParseIntPipe) id: number,
-    @Res() res: Response,
-  ) {
-    const userData = await this.userService.getUserData(id)
-    if(userData === null) {
-     throw new HttpException('User with id = ' + id + ' not found', HttpStatus.NOT_FOUND)
-    }
-    await this.userService.deleteUser(id)
-    return res.send({ status: 'ok' })
-  }
+  // @Delete('/:id')
+  // @ApiResponse({ 
+  //   status: 200, 
+  //   description: 'Delete user.'
+  // })
+  // @ApiResponse({ 
+  //   status: 404, 
+  //   description: 'User not Found.'
+  // })
+  // async deleteUser(
+  //   @Param('id', ParseIntPipe) id: number,
+  //   @Res() res: Response,
+  // ) {
+  //   const userData = await this.userService.getUserData(id)
+  //   if(userData === null) {
+  //    throw new HttpException('User with id = ' + id + ' not found', HttpStatus.NOT_FOUND)
+  //   }
+  //   await this.userService.deleteUser(id)
+  //   return res.send({ status: 'ok' })
+  // }
 }
--- src/entities/user/user.service.ts
+++ src/entities/user/user.service.ts
@@ -2,72 +2,73 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { genSalt, hash } from 'bcrypt'
+import { TypeOrmCrudService } from "@nestjsx/crud-typeorm";
 
 import { User } from './user.entity'
 import { UpdateUserDto } from './dto/updateUser.dto'
 
 
 @Injectable()
-export class UserService {
-  constructor(
-    @InjectRepository(User) private readonly userRepository: Repository<User>,
-  ) {}
-
-  availableFields = [
-    'nameFirst',
-    'nameLast',
-    'email',
-    'gender',
-    'birthDate',
-  ]
-
-  private filterFields(body: { [k: string]: any }) {
-    const filteredBody: { [k: string]: any } = {}
-
-    Object.keys(body).filter((k) => {
-      if (this.availableFields.includes(k)) {
-        filteredBody[k] = body[k]
-      }
-    })
-
-    return filteredBody
+export class UserService extends TypeOrmCrudService<User> {
+  constructor(@InjectRepository(User) userRepository) {
+      super(userRepository)
   }
 
-  public async createUser(userData: any) {
+  // availableFields = [
+  //   'nameFirst',
+  //   'nameLast',
+  //   'email',
+  //   'gender',
+  //   'birthDate',
+  // ]
 
-    const salt = await genSalt(10)
+  // private filterFields(body: { [k: string]: any }) {
+  //   const filteredBody: { [k: string]: any } = {}
 
-    const hashedPassword = await hash(userData.password, salt)
+  //   Object.keys(body).filter((k) => {
+  //     if (this.availableFields.includes(k)) {
+  //       filteredBody[k] = body[k]
+  //     }
+  //   })
 
-    const newUser = this.userRepository.create({
-      ...userData,
-      password: hashedPassword,
-    })
+  //   return filteredBody
+  // }
 
-    return await this.userRepository.save(newUser)
-  }
+  // public async createUser(userData: any) {
 
-  public async getAllUsers() {
-    return await this.userRepository.find({
-      select: this.availableFields as any
-    })
-  }
+  //   const salt = await genSalt(10)
 
-  public async getUserData(id: number) {
-    return await this.userRepository.findOne({
-      where: { id },
-      select: this.availableFields as any
-    })
-  }
+  //   const hashedPassword = await hash(userData.password, salt)
 
-  public async updateUserData(id: number, body: UpdateUserDto) {
-    return await this.userRepository.update(
-      { id },
-      this.filterFields(body)
-    )
-  }
+  //   const newUser = this.userRepository.create({
+  //     ...userData,
+  //     password: hashedPassword,
+  //   })
 
-  public async deleteUser(id: number) {
-    return await this.userRepository.delete(id)
-  }
+  //   return await this.userRepository.save(newUser)
+  // }
+
+  // public async getAllUsers() {
+  //   return await this.userRepository.find({
+  //     select: this.availableFields as any
+  //   })
+  // }
+
+  // public async getUserData(id: number) {
+  //   return await this.userRepository.findOne({
+  //     where: { id },
+  //     select: this.availableFields as any
+  //   })
+  // }
+
+  // public async updateUserData(id: number, body: UpdateUserDto) {
+  //   return await this.userRepository.update(
+  //     { id },
+  //     this.filterFields(body)
+  //   )
+  // }
+
+  // public async deleteUser(id: number) {
+  //   return await this.userRepository.delete(id)
+  // }
 } 
